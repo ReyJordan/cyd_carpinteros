5d6d6b229f19edb67ef96acfe88c7e917940a7e6
ReyJordan <reyjai94@hotmail.com>
2023-08-15 08:16:13 -0500

    prueba 2

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const slider = document.querySelector('.slider');
-const slides = document.querySelectorAll('.slide');
+const slides = document.querySelectorAll('.slides');
 let currentIndex = 0;
 
 function showSlide(index) {
   if (index < 0) {
-    index = slides.length - 1;
-  } else if (index >= slides.length) {
+    index = slides.chilElementCount - 1;
+  } else if (index >= slides.childElementCount) {
     index = 0;
   }
 
-  slider.style.transform = `translateX(-${index * 100}%)`;
+  slider.style.transform = `translateX(-${index * 20}%)`;
   currentIndex = index;
 }
 
